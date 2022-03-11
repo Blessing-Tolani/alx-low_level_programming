c1d0a443af6ca03202b867b90583455c6a5055fd
Blessing-Tolani <ojoblessing999@gmail.com>
2022-03-11 11:54:45 -0800

    solve task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - prints alphabet for insomnia patients
+ * Return: Always 0
+ */
+int main(void)
+{
+	char arr[] = "abcdefghijklmnopqrstuvwxyz";
+
+	int i;
+
+	for(i=0; i<27; i++)
+	{
+		putchar(arr[i])
+	}
+
+	return (0);
+}
