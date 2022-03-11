aa70cadbb2d18563427780f93985e470fad425c7
Blessing-Tolani <ojoblessing999@gmail.com>
2022-03-11 15:53:30 -0800

    solve task 12

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+ * main - prints all possible combinations of two two-digit numbers
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i, j, k, m;
+	for (i = '0'; i <= '9'; i++)
+	{
+		for (j = '0'; j <= '8'; j++)
+		{
+			for (k = '0'; k < '9'; k++)
+			{
+				for (m = k + 1; m <= '9'; m++)
+				{
+					putchar(i);
+					putchar(j);
+					putchar(' ')
+					putchar(k);
+					putchar(m)
+					if (i == '9' && j == '8' && k == '9' && 					m == "9")
+					continue;
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
