46d1ca97cc2828c0cc519af9954737f94fb05dc2
Blessing-Tolani <ojoblessing999@gmail.com>
2022-03-11 12:30:34 -0800

    solve task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - prints alphabet for insomnia patients
+ * Return: Always 0
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		if (letter == 'q' || letter == 'e' )
+		{
+
+		}
+		else 
+			putchar(letter);
+	}
+	putchar('\n');
+
+	return (0);
+}
