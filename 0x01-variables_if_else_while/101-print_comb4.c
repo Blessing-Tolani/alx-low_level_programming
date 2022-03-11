3c68a173520281c276a2529748a3605b0ecb54ae
Blessing-Tolani <ojoblessing999@gmail.com>
2022-03-11 14:46:39 -0800

    correct task 11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -12,14 +12,14 @@ int main(void)
 	int k;
 
 	for (i = '0'; i < '8'; i++)
-	{	
+	{
 		for (j = i + 1; j < '9'; j++)
-		{	
+		{
 			for (k = j + 1; k <= '9'; k++)
 			{
 				putchar(i);
 				putchar(j);
-				putchar(k);	
+				putchar(k);
 				if (i == '7' && j == '8' && k == '9')
 				continue;
 				putchar(',');
