762f60bd2614877bd53089ab73805c7aa0ec556c
Blessing-Tolani <ojoblessing999@gmail.com>
2022-03-11 06:40:11 -0800

    correct task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 /** 
- *Prints predefined text
- *returns 0
+ * main - entry point for c
+ * Return: always 0
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
