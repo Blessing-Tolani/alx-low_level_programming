7f4da46af29ae57db4642320c96fd1ebfc8fe711
Blessing-Tolani <ojoblessing999@gmail.com>
2022-03-11 16:02:10 -0800

    correct task 12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -20,7 +20,7 @@ int main(void)
 					putchar(' ');
 					putchar(k);
 					putchar(m);
-					if (i, k, m == '9' && j == '8')
+					if (i == '9' && k == '9' && m == '9' && j == '8')
 					continue;
 					putchar(',');
 					putchar(' ');
