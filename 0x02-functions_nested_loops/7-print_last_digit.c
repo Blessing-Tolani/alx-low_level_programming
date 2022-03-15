f86215af5e9853a6b393ae5a3f8fd03e52cf1b14
Blessing-Tolani <ojoblessing999@gmail.com>
2022-03-15 08:54:04 -0700

    fix: correct task 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,13 +10,17 @@
  */
 int print_last_digit(int n)
 {
+	int s1;
+
+	int double_last_digit;
+
 	if (n >= 0)
 	{
 		int last_digit = n % 10;
 
 		sprintf(s1,"%d", last_digit);
 		strcat(s1, s1);
-		int double_last_digit = atoi(s1);
+		double_last_digit = atoi(s1);
 
 	}
 	else if (n < 0)
@@ -26,7 +30,7 @@ int print_last_digit(int n)
 
 		sprintf(s1,"%d", last_digit);
 		strcat(s1, s1);
-		int double_last_digit = atoi(s1);
+		double_last_digit = atoi(s1);
 	
 	}
 	return (double_last_digit);
