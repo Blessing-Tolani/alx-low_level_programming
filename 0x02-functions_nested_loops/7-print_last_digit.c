0cbec22a55c748852d15dca333a20e9fffba60dd
Blessing-Tolani <ojoblessing999@gmail.com>
2022-03-15 08:51:52 -0700

    fix: correct task 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,14 +1,33 @@
 #include "main.h"
-#include <ctype.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 /**
  * print_last_digit - prints last digit of a number
  * @n: the number
  *
- * Return: returns last digit
+ * Return: returns double last digit
  */
 int print_last_digit(int n)
 {
-	int last_digit = n % 10;
+	if (n >= 0)
+	{
+		int last_digit = n % 10;
 
-	return (last_digit);
+		sprintf(s1,"%d", last_digit);
+		strcat(s1, s1);
+		int double_last_digit = atoi(s1);
+
+	}
+	else if (n < 0)
+	{
+		int a = 0-n;
+		int last_digit = a % 10;
+
+		sprintf(s1,"%d", last_digit);
+		strcat(s1, s1);
+		int double_last_digit = atoi(s1);
+	
+	}
+	return (double_last_digit);
 }
