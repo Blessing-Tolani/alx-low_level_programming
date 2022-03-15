c4b0c8cb0885ca871935857583336ea3517e5bd0
Blessing-Tolani <ojoblessing999@gmail.com>
2022-03-15 06:06:21 -0700

    featt: solve task 2

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * main - program that prints _putchar
+ * print_alphabet - program that prints the alphabet
  * Return: Always 0
  */
 int print_alphabet(void)
@@ -9,6 +9,8 @@ int print_alphabet(void)
 
 	for (letter = 'a'; letter <= 'z'; letter++)
 		_putchar(letter);
+
+	_putchar('\n');
 
 	return (0);
 }
---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - program that prints the alphabet 10 times
+ * Return: Always 0
+ */
+int print_alphabet_x10(void)
+{
+	int a = 0;
+
+	while(a <= 9)
+	{
+		char letter;
+
+		for (letter = 'a'; letter <= 'z'; letter++)
+			_putchar(letter);
+		
+		_putchar('\n');
+		a++;
+	}
+
+	return (0);
+}
