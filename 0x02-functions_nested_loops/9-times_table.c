57f3eea60a00a3a87e630aa174eec3daadcff2e9
Blessing-Tolani <ojoblessing999@gmail.com>
2022-03-15 14:36:50 -0700

    feat: solve task 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * times_table - prints the 9 times table starting with 0
+ */
+void times_table(void)
+{
+	int a;
+
+	int b;
+
+	for (a = 0; a <= 9; a++)
+	{
+		for (b = 0; b <= 9; b++)
+		{
+			int ans = a * b;
+
+			_putchar((1 % 10) + '0');
+			if (b == 9)
+				continue;
+			_putchar(',');
+			_putchar(' ');
+		}
+		_putchar('\n');
+	}
+}
