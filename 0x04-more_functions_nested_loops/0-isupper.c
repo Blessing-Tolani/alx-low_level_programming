d1b8b7410e5f063d551f0396d0e7bd5232681354
Blessing-Tolani <ojoblessing999@gmail.com>
2022-03-17 19:40:27 -0700

    feat: solve task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <ctype.h>
+/**
+ *_isupper - checks for uppercase character
+ * @c: the character to be checked
+ *
+ * Return: returns 1 or 0
+ */
+int _isupper(int c)
+{
+	int number = isupper(arg);
+
+	if (number > 0)
+		return (1);
+	else
+		return (0);
+}
