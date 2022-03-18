46258ccd6c6047a025555c44b6258433284723ee
Blessing-Tolani <ojoblessing999@gmail.com>
2022-03-17 20:48:20 -0700

    feat: solve task 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_most_numbers - program that prints numbers
+ */
+void print_most_numbers(void)
+{
+	int number;
+
+	for (number = 0; number <= 9; number++)
+	{
+		if (number = 2 || number = 4)
+			continue;
+		_putchar(number + '0');
+	}
+	_putchar('\n');
+}
