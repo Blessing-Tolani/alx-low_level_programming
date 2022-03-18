2f3be14c311cc37bd5d7e99e14dd9e5264f8326e
Blessing-Tolani <ojoblessing999@gmail.com>
2022-03-18 05:25:31 +0100

    fix

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -4,19 +4,20 @@
  */
 void more_numbers(void)
 {
-  int a = 0;
-  
-  while (a <= 10)
-  {
-	int number;
-	for (number = 0; number <= 14; number++)
-	{
-		if (number > 9)
-			_putchar((number / 10) + '0');
-		_putchar((number % 10) + '0');
-	}
-	_putchar('\n');
-	a++;
+	int a = 0;
+	
+	while (a <= 10)
+  	{
+		int number;
+		
+		for (number = 0; number <= 14; number++)
+		{
+			if (number > 9)
+				_putchar((number / 10) + '0');
+			_putchar((number % 10) + '0');
+		}
+		_putchar('\n');
+		a++;
 	}
 }
 	
