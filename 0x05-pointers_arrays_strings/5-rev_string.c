0f683aafeca2b0f74abaa4e1975181b7f4966b84
Blessing-Tolani <ojoblessing999@gmail.com>
2022-03-22 12:14:19 -0700

    feat: solve task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <string.h>
+/**
+ * rev_string - reverse a string
+ * @s: string
+ *
+ * Return: 0
+ */
+void rev_string(char *s)
+{
+	int len = strlen(s);
+
+	char p[len + 1];
+
+	int c;
+
+	p[len] = '\0';
+
+	for (c = 0; c < len; c++)
+		p[c] = s[len - 1 - c];
+	strcpy(s, p);
+}
