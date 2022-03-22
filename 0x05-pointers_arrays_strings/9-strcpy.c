03cb00c9e9f6f62100951145387a1f86bd035641
Blessing-Tolani <ojoblessing999@gmail.com>
2022-03-22 14:18:30 -0700

    solve task 9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strcpy - copy one string to another
+ * @dest: destination
+ * @src: source
+ *
+ * Return: return string in destination
+ */
+ char *_strcpy(char *dest, char *src)
+{
+	dest = strcpy(dest, src);
+	return (dest);
+}
