448cf0ccefcea3fd057af0bf70a3580f32856d3a
Blessing-Tolani <ojoblessing999@gmail.com>
2022-03-24 12:46:01 -0700

    feat: solve task 4

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,42 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reverse_array - reverses integers in an array
+ * @a: array
+ * @n: size of array
+ */
+void reverse_array(int *a, int n)
+{
+	int i;
+
+	int b = n - 1;
+
+	int c;
+
+	i = b;
+	while (i >= 0)
+	{
+		if (i != b)
+			_putchar(',');
+		c = a[i];
+		if (c > 10 && c < 99)
+		{
+			_putchar((c / 10) + '0');
+		}
+		if (c > 99 && c < 999)
+		{
+			_putchar(((c / 10) / 10) + '0');
+			_putchar(((c / 10) % 10) + '0');
+		}
+		if (c > 999)
+		{
+			_putchar((((c / 10) / 10) / 10) + '0');
+			_putchar((((c / 10) / 10) % 10) + '0');
+			_putchar(((c / 10) % 10) + '0');
+		}
+		_putchar((c % 10) + '0');
+		i--;
+	}
+	_putchar('\n');
+}
