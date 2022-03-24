e49bc01dda3f93a946270a42965526cdfa7f06ac
Blessing-Tolani <ojoblessing999@gmail.com>
2022-03-24 05:11:26 -0700

    solve task 0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcat - return concatenated strings
+ * @dest: destination string
+ * @src: source string
+ *
+ * Return: string
+ */
+char *_strcat(char *dest, char *src)
+{
+	return (strcat(dest, str));
+}
