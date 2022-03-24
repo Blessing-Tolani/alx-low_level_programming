d3b7d1b0636f73fe4f25c4abe187f811db547fc0
Blessing-Tolani <ojoblessing999@gmail.com>
2022-03-24 06:53:44 -0700

    feat: solve task 1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strncat - return concatenated strings
+ * @dest: destination string
+ * @src: source string
+ * @n: no of bytes
+ *
+ * Return: string
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int a = 0;
+	int b = 0;
+
+	while (dest[a] != '\0')
+		a++;
+	while (src[b] != '\0' && b < n)
+	{
+		dest[a] = src[b];
+		b++;
+		a++;
+	}
+	return (dest);
+}
