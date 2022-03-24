1e34dcd83994f59b1f4c9dcdd2aca6f02da9600a
Blessing-Tolani <ojoblessing999@gmail.com>
2022-03-24 13:59:18 -0700

    solve task 5

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,6 +1,4 @@
 #include "main.h"
-#include <stdio.h>
-
 /**
  * reverse_array - reverses integers in an array
  * @a: array
---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * string_toupper - converts lovercase to uppercase
+ * @s: string
+ */
+char *string_toupper(char *s)
+{
+	int a = 0;
+	
+	while (s[a] != '\0')
+	{
+		if (s[a] >= 'a' && s[a] <= 'z')
+			s[a] = s[a] - 32;
+		a++;
+	}
+	return s;
+}
