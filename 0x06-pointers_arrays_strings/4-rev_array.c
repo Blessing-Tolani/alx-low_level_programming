d5931e75ebc178f5c5fb64e28f8e71340b1d2d0d
Blessing-Tolani <ojoblessing999@gmail.com>
2022-03-24 12:51:45 -0700

    fix

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -5,20 +5,22 @@
  * reverse_array - reverses integers in an array
  * @a: array
  * @n: size of array
+ *
  */
 void reverse_array(int *a, int n)
 {
 	int i;
-
 	int b = n - 1;
-
 	int c;
 
 	i = b;
 	while (i >= 0)
 	{
 		if (i != b)
+		{
 			_putchar(',');
+			_putchar(' ');
+		}
 		c = a[i];
 		if (c > 10 && c < 99)
 		{
