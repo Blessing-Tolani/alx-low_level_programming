a7c1a4b0d68829269f68cfe0371a9097603a9ba2
Blessing-Tolani <ojoblessing999@gmail.com>
2022-03-25 04:39:12 +0100

    feat: solve task 7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * leet - Encodes a string to 1337.
+ * @str: The string to be encoded.
+ *
+ * Return: A pointer to the encoded string.
+ */
+char *leet(char *str)
+{
+  int a= 0;
+  
+  int b;
+  
+  char smallLeet[5] = {'a', 'e', 'o', 't', 'l'};
+  
+  char capLeet[5] = {'A', 'E', 'O', 'T', 'L'};
+  
+  int num[5] = {4, 3, 0, 7, 1};
+    
+  while(str[a])
+  {
+    for(b = 0; b < 5; b++)
+    {
+      if (str[a] == smallLeet[b] || str[a] == capLeet[b])
+        str[a] = num[b] + '0';
+    }
+    a++:
+  }
+  return (str);
+}
