c57c8986b6f5b91b238e6f7ca3090265300e9be4
Blessing-Tolani <ojoblessing999@gmail.com>
2022-03-26 05:29:14 +0100

    fix

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,25 +1,47 @@
-#include "main.h"
-#include <string.h>
+#include <stdio.h>
 
 /**
- * _strcat - return concatenated strings
- * @dest: destination string
- * @src: source string
- *
- * Return: string
+ * print_buffer - Print the entire buffer w/ certain conditions
+ * @b: The buffer to print
+ * @size: The size of the buffer
  */
-char *_strcat(char *dest, char *src)
+void print_buffer(char *b, int size)
 {
-	int a = 0;
-	int b = 0;
+	int i, j;
 
-	while (dest[a] != '\0')
-		a++;
-	while (src[b] != '\0')
+	i = 0;
+	if (size <= 0)
+		putchar('\n');
+	else
 	{
-		dest[a] = src[b];
-		b++;
-		a++;
+		while (i < size)
+		{
+			printf("%08x: ", i);
+			j = 0;
+			while (j < 10)
+			{
+				if (j % 2 == 0 && j > 0)
+					printf(" ");
+				if (j + i > size - 1)
+					printf("  ");
+				else
+					printf("%.2x", b[j + i]);
+				j++;
+			}
+			putchar(' ');
+			j = 0;
+			while (j < 10)
+			{
+				if (j + i > size - 1)
+					break;
+				if (b[j + i] >= ' ' && b[j + i] <= '~')
+					putchar(b[j + i]);
+				else
+					putchar('.');
+				j++;
+			}
+			putchar('\n');
+			i += 10;
+		}
 	}
-	return (dest);
 }
