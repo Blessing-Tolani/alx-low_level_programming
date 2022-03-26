a16d85600dedb6d294f5c506006c1d766361a168
Blessing-Tolani <ojoblessing999@gmail.com>
2022-03-26 05:12:44 +0100

    solve task 102

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,25 +1,22 @@
-#include "main.h"
-#include <string.h>
-
-/**
- * _strcat - return concatenated strings
- * @dest: destination string
- * @src: source string
- *
- * Return: string
- */
-char *_strcat(char *dest, char *src)
+#include <stdio.h>
+int main(void)
 {
-	int a = 0;
-	int b = 0;
+  int n;
+  int a[5];
+  int *p;
 
-	while (dest[a] != '\0')
-		a++;
-	while (src[b] != '\0')
-	{
-		dest[a] = src[b];
-		b++;
-		a++;
-	}
-	return (dest);
+  a[2] = 1024;
+  p = &n;
+  /*
+   * write your line of code here...
+   * Remember:
+   * - you are not allowed to use a
+   * - you are not allowed to modify p
+   * - only one statement
+   * - you are not allowed to code anything else than this line of code
+   */
+   *(p + 5) = 98;
+  /* ...so that this prints 98\n */
+  printf("a[2] = %d\n", a[2]);
+  return (0);
 }
