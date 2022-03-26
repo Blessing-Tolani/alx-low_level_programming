bc22e949badbcf421cfdf8a6cab6f9a8a2aec1dc
Blessing-Tolani <ojoblessing999@gmail.com>
2022-03-25 21:19:48 -0700

    solve

---
+++ 0x06-pointers_arrays_strings/10-infinite_add.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcat - return concatenated strings
+ * @dest: destination string
+ * @src: source string
+ *
+ * Return: string
+ */
+char *_strcat(char *dest, char *src)
+{
+	int a = 0;
+	int b = 0;
+
+	while (dest[a] != '\0')
+		a++;
+	while (src[b] != '\0')
+	{
+		dest[a] = src[b];
+		b++;
+		a++;
+	}
+	return (dest);
+}
