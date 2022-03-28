8ec231f4030af5146b69c064bb4cb564a71ac3de
Blessing-Tolani <ojoblessing999@gmail.com>
2022-03-28 04:47:45 -0700

    fix

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -8,7 +8,7 @@
  * 
  * Return: a pointer to memory area
  */
-char _memset(char *s, char b, unsigned int n)
+char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
 
--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -8,7 +8,7 @@
  * 
  * Return: returns destination
  */
-char _memcpy(char *dest, char *src, unsigned int n)
+char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
 
