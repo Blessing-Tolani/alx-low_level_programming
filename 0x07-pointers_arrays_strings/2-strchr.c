2669ee404680f00ea32103fba1be482d82d0d9db
Blessing-Tolani <ojoblessing999@gmail.com>
2022-03-28 11:58:16 -0700

    solve task 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strchr - locates a character in a string
+ * @s: string
+ * @c: character
+ *
+ * Return: a pointer to memory area
+ */
+char *_strchr(char *s, char c)
+{
+	unsigned int i;
+
+	i = 0;
+	while (i < n)
+	{
+		s[i] = b;
+		i++:
+	}
+	return (s);
+}
