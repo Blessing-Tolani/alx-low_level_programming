1b74bc95e1ae586fc013c0ce0a5920b85a1360e7
Blessing-Tolani <ojoblessing999@gmail.com>
2022-03-29 05:05:21 +0100

    fix

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,22 +1,11 @@
 #include "main.h"
 
 /**
- * _memset - copies byte b to memory
- * @s: memory area
- * @b: byte
- * @n: no of bytes to replace
- * 
- * Return: a pointer to memory area
+ * set_string - sets the value of a pointer to a char
+ * @s: pointer
+ * @to: char
  */
-char _memset(char *s, char b, unsigned int n)
+void set_string(char **s, char *to)
 {
-	unsigned int i;
-
-	i = 0;
-	while (i < n)
-	{
-		s[i] = b;
-		i++:
-	}
-	return (s);
+	*s = to;
 }
