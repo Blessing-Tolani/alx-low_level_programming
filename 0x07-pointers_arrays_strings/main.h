0c9dedf8f73f05046967fe7a9bbd6f9832d3dabd
Blessing-Tolani <ojoblessing999@gmail.com>
2022-03-29 21:27:35 +0100

    update

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -6,3 +6,4 @@ int _putchar(char c);
 void print_chessboard(char (*a)[8]);
 void set_string(char **s, char *to);
 char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
