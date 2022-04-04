3366d220c0ae633b98748c2d44d56d112f0f1d8c
Blessing-Tolani <ojoblessing999@gmail.com>
2022-04-04 14:10:11 -0700

    feat: solve task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints name
+ * @argc: parameter 1
+ * @argv: parameter array
+ * Return: return 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+	(void)argv;
+	return (0);
+}
