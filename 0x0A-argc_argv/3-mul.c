4251f1620f7eaeee3af47fc3091ee2cb8c381084
Blessing-Tolani <ojoblessing999@gmail.com>
2022-04-04 14:41:24 -0700

    feat: solve task 3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints name
+ * @argc: argument count
+ * @argv: array of strings
+ * Return: return 0
+ */
+int main(int argc, char *argv[])
+{
+	int count, sum = 1;
+
+	if (argc > 1)
+	{
+		for (count = 1; count < argc; count++)
+		{
+			sum *= atoi(argv[count]);
+		}
+		printf("%d\n", sum);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+	return (0);
+}
