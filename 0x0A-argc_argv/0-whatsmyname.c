cc9ece27d50228da00a055c16867c3b69ca881b9
Blessing-Tolani <ojoblessing999@gmail.com>
2022-04-04 13:51:13 -0700

    solve task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - prints name
+ * @argc: parameter 1
+ * @argv: parameter array
+ * Return: return 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s", argv[0]);
+	return (0);
+}
