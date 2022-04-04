d712cd2083322182b11c3a774a54f2b71e0f4631
Blessing-Tolani <ojoblessing999@gmail.com>
2022-04-04 14:21:41 -0700

    feat: solve task 2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints name
+ * @argc: argument count
+ * @argv: array of strings
+ * Return: return 0
+ */
+int main(int argc, char *argv[])
+{
+	int count;
+
+	for (count = 0; count < argc; count++)
+		printf("%s\n", argv[count]);
+	printf("\n");
+	return (0);
+}
