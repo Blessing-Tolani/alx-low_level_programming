0d843ad890492c1cee8bc5b06eb5714feac10758
Blessing-Tolani <ojoblessing999@gmail.com>
2022-04-06 02:33:12 +0100

    fix: task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,47 +1,54 @@
 #include "main.h"
+#include <stdio.h>
 #include <stdlib.h>
 
-char *_strcpy(char *dest, char *src);
+int _strlen_recursion(char *s);
 
 /**
- * _strdup - Function that returns a pointer to a new string
- * @str: String
- * Return: Pointer to a new string
+ * *str_concat - Concatenates two strings
+ * @s1: String
+ * @s2: String
+ * Return: Contents of s1 followed by s2 and null terminated
  */
-char *_strdup(char *str)
+char *str_concat(char *s1, char *s2)
 {
-	char *dupli, *copy;
-	int len = 0;
+	char *new, *s1cp, *s2cp;
+	int len1 = 0, len2 = 0, i, j = 0;
 
-	if (str == NULL)
+	s1cp = s1;
+	s2cp = s2;
+	if (s1 != NULL)
+		len1 = _strlen_recursion(s1cp);
+	if (s2 != NULL)
+		len2 = _strlen_recursion(s2cp);
+
+	new = (char *) malloc(sizeof(char) * len1 + len2 + 1);
+
+	if (new == NULL)
 		return (NULL);
-	for (copy = str; *copy != '\0'; copy++)
-		len++;
-	dupli = malloc(len + 1);
-	if (dupli == NULL)
-		return (NULL);
-	_strcpy(dupli, str);
-	return (dupli);
+
+	for (i = 0; i < len1; i++)
+		new[i] = s1[i];
+
+	for (i = len1; i < len1 + len2; i++)
+	{
+		new[i] = s2[j];
+		j++;
+	}
+
+	new[i] = '\0';
+
+	return (new);
 }
 
 /**
- * *_strcpy - copy the pointed string
- * @dest: pointer of a char[] variable
- * @src: sames as dest
- * Return: copy of the pointer char
+ * _strlen_recursion - Length of a string
+ * @s: char pointer
+ * Return: Integer variable
  */
-char *_strcpy(char *dest, char *src)
+int _strlen_recursion(char *s)
 {
-	int counter = 0;
-
-	while (*src != '\0')
-	{
-		*dest = *src;
-		dest++;
-		src++;
-		counter++;
-	}
-	*dest = '\0';
-	dest -= counter;
-	return (dest);
+	if (*s != '\0')
+		return (1 + _strlen_recursion(++s));
+	return (0);
 }
