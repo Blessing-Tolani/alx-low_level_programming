e038312e56dd73d45a807caa95e7f77e5904206b
Blessing-Tolani <ojoblessing999@gmail.com>
2022-04-08 02:06:41 +0100

    fix: task 0

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,22 +1,17 @@
 #include "main.h"
-#include <stdlib.h>
-/**
- * *create_array - Function that createss arrays of chars
- * @size: Size of the char
- * @c: char to be initialized
- * Return: Array of chars
- */
-char *create_array(unsigned int size, char c)
-{
-	char *ptc;
-	unsigned int i;
 
-	if (size == 0)
-		return (NULL);
-	ptc = (char *) malloc(size * (sizeof(char)));
-	if (ptc == NULL)
-		return (NULL);
-	for (i = 0; i < size; i++)
-		ptc[i] = c;
-	return (ptc);
+/**
+ * malloc_checked - allocates memory using malloc
+ * @b: bytes allocate
+ * Return: pointer
+ * code written by Johnel
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *ptr;
+
+	ptr = malloc(b);
+	if (ptr == NULL)
+		exit(98);
+	return (ptr);
 }
