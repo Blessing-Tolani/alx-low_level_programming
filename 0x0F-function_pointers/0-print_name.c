5b3dd856976703c655eabbd7a1a78ac23ca55c12
Blessing-Tolani <ojoblessing999@gmail.com>
2022-04-13 15:14:56 -0700

    chore: edit header file

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -3,9 +3,9 @@
  * print_name - function that prints a name
  * @name: parameter name
  * @f: function pointer
- * Return: Nothing
  */
 void print_name(char *name, void (*f)(char *))
 {
-	f(name);
+	if (name != NULL && f != NULL)
+		f(name);
 }
