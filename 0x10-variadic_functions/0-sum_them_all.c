803f4be7e46cb8cad2bb15aaf4ce6f685ed8073d
Blessing-Tolani <ojoblessing999@gmail.com>
2022-04-14 16:30:23 -0700

    feat: task on variadic functions

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,11 @@
+#include <stdarg.h>
+/**
+ * sum_them_all - variadic function
+ * @n: const
+ * Return: int
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list list;
+}
