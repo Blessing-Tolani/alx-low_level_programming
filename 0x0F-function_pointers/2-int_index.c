0bcf2eb4833f9797f9596d04f05d8ecff0a5d073
Blessing-Tolani <ojoblessing999@gmail.com>
2022-04-13 21:08:27 -0700

    fix: task 2

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,22 @@
+#include "function_pointers.h"
+/**
+ * int_index - searches for an integer
+ * @array: array
+ * @size: size of array
+ * @cmp: pointer to the function used
+ * Return: an integer
+ */
+int int_index(int *array, size_t size, void (*cmp)(int))
+{
+	unsigned long i;
+
+	if (size > 0 && array != NULL || cmp != NULL)
+	{
+		for (i = 0; i < size; i++)
+		{
+			if (cmp(array[i]) != 0)
+				return (i);
+		}
+	}
+	return (-1);
+}
