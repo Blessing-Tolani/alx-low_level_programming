a446d415421f7cdef9f68a0cebd0f4b4a11c1d3b
Blessing-Tolani <ojoblessing999@gmail.com>
2022-04-14 05:33:31 +0100

    fix: task 100

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,18 +1,39 @@
-#include "function_pointers.h"
-/**
- * array_iterator - execute a funtion on each element of an array
- * @array: array
- * @size: size of array
- * @action: function pointer
- */
-void array_iterator(int *array, size_t size, void (*action)(int))
-{
-	unsigned long i;
+##include <stdlib.h>
+#include <stdio.h>
 
-	if (array == NULL || action == NULL)
-		return;
-	for (i = 0; i < size; i++)
+
+/**
+ * main - Print opcodes from main
+ * @argc: size of argv
+ * @argv: Array of strings
+ *
+ * Return: 0 on success
+ */
+int main(int argc, char  **argv)
+{
+	int num, i;
+
+	if (argc != 2)
 	{
-		action(array[i]);
+		printf("Error\n");
+		exit(1);
 	}
+	num = atoi(argv[1]);
+
+	if (num < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	for (i = 0; i < num; i++)
+	{
+		printf("%02hhx", ((char *)main)[i]);
+		if (i != num - 1)
+			printf(" ");
+	}
+
+	printf("\n");
+
+	return (0);
 }
