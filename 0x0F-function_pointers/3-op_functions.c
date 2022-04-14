c38499b28dcbd4b0b73c79e30d623ce05e09ffa7
Blessing-Tolani <ojoblessing999@gmail.com>
2022-04-14 05:32:48 +0100

    fix: task 3

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,18 +1,61 @@
-#include "function_pointers.h"
-/**
- * array_iterator - execute a funtion on each element of an array
- * @array: array
- * @size: size of array
- * @action: function pointer
- */
-void array_iterator(int *array, size_t size, void (*action)(int))
-{
-	unsigned long i;
+#include "3-calc.h"
 
-	if (array == NULL || action == NULL)
-		return;
-	for (i = 0; i < size; i++)
-	{
-		action(array[i]);
-	}
+/**
+ * op_add - Sum two numbers
+ * @a: Integer
+ * @b: Integer
+ *
+ * Return: a + b
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - Difference of two numbers
+ * @a: Integer
+ * @b: Integer
+ *
+ * Return: a - b
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - Product of two numbers
+ * @a: Integer
+ * @b: Integer
+ *
+ * Return: a * b
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - Division of two numbers
+ * @a: Integer
+ * @b: Integer
+ *
+ * Return: a / b
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - Remainder of two numbers
+ * @a: Integer
+ * @b: Integer
+ *
+ * Return: a % b
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
 }
