d62f896a72eb24692bb0a96c8642dbeecafa944a
Blessing-Tolani <ojoblessing999@gmail.com>
2022-04-14 05:31:58 +0100

    fix: task 3

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,19 +1,38 @@
 
-#include "function_pointers.h"
-/**
- * array_iterator - execute a funtion on each element of an array
- * @array: array
- * @size: size of array
- * @action: function pointer
- */
-void array_iterator(int *array, size_t size, void (*action)(int))
-{
-	unsigned long i;
+#include "3-calc.h"
 
-	if (array == NULL || action == NULL)
-		return;
-	for (i = 0; i < size; i++)
+
+/**
+ * main - Emulate a calculator in the shell
+ * @argc: Size of argv
+ * @argv: Array of string of parameters given in the shell
+ *
+ * Return: 0 on success
+ */
+int main(int argc, char **argv)
+{
+	int num1, num2;
+
+	if (argc != 4)
 	{
-		action(array[i]);
+		printf("Error\n");
+		exit(98);
 	}
+
+	if (get_op_func(argv[2]) == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	if ((*argv[2] == '/' || *argv[2] == '%') && (num2 == 0))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	printf("%d\n", get_op_func(argv[2])(num1, num2));
+
+	return (0);
 }
