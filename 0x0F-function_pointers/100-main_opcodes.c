791910e9e7cb93e3467d24e5e83ac23e0380d18d
Blessing-Tolani <ojoblessing999@gmail.com>
2022-04-13 21:28:39 -0700

    solve remaining tasks

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,18 @@
+#include "function_pointers.h"
+/**
+ * array_iterator - execute a funtion on each element of an array
+ * @array: array
+ * @size: size of array
+ * @action: function pointer
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned long i;
+
+	if (array == NULL || action == NULL)
+		return;
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+}
