b2cbeb1f6214dea6d40414b89c930a01ae656ecd
Blessing-Tolani <ojoblessing999@gmail.com>
2022-04-14 05:29:44 +0100

    fix: task 3

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,18 +1,28 @@
-#include "function_pointers.h"
-/**
- * array_iterator - execute a funtion on each element of an array
- * @array: array
- * @size: size of array
- * @action: function pointer
- */
-void array_iterator(int *array, size_t size, void (*action)(int))
-{
-	unsigned long i;
+##ifndef CALC_H
+#define CALC_H
 
-	if (array == NULL || action == NULL)
-		return;
-	for (i = 0; i < size; i++)
-	{
-		action(array[i]);
-	}
-}
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * struct op - Struct op
+ *
+ * @op: The operator
+ * @f: The function associated
+ */
+typedef struct op
+{
+	char *op;
+	int (*f)(int a, int b);
+
+} op_t;
+
+int op_add(int a, int b);
+int op_sub(int a, int b);
+int op_mul(int a, int b);
+int op_div(int a, int b);
+int op_mod(int a, int b);
+
+int (*get_op_func(char *s))(int, int);
+
+#endif
