85b0d194c15adf238694c24f58e1a661822f8084
Blessing-Tolani <ojoblessing999@gmail.com>
2022-04-15 00:40:13 +0100

    fix: task 2

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -1,11 +1,28 @@
 #include <stdarg.h>
+#include <stdio.h>
 /**
- * sum_them_all - variadic function
- * @n: const
- * Return: int
+ * print_strings - print strings
+ * @separator:char
+ * @n:unsigned int
+ * Return:void
  */
-int sum_them_all(const unsigned int n, ...)
+void print_strings(const char *separator, const unsigned int n, ...)
 {
-	unsigned int i;
 	va_list list;
+	unsigned int i;
+	char *str;
+
+	va_start(list, n);
+	for (i = 0; i < n; i++)
+	{
+		str = va_arg(list, char *);
+		if (str)
+			printf("%s", str);
+		else
+			printf("(nil)");
+		if (i < n - 1 && separator)
+			printf("%s", separator);
+	}
+	va_end(list);
+	printf("\n");
 }
