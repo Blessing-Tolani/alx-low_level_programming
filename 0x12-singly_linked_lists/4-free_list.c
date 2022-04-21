3e5faec826964312550d21071a59aa8ea6797b25
Blessing-Tolani <ojoblessing999@gmail.com>
2022-04-21 21:59:55 +0100

    update

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,26 +1,19 @@
 #include "lists.h"
-/**
- * add_node - adds a new node at the beginning of the list
- * @head: pointer  to the beginning
- * @str: str to be duplicated
- * Return: address of the new element or null
- */
-list_t *add_node(list_t **head, const char *str)
-{
-	list_t *new;
-	int count = 0;
 
-	new = malloc(sizeof(list_t));
-	if (new == NULL)
+/**
+ * free_list - frees a list
+ * @head: head of the linked list.
+ * Return: no return.
+ */
+
+void free_list(list_t *head)
+{
+	list_t *current;
+
+	while ((current = head) != NULL)
 	{
-		printf("Error\n");
-		return (NULL);
+		head = head->next;
+		free(current->str);
+		free(current);
 	}
-	new->str = strdup(str);
-	for (count = 0; str[count] != '\0'; count++)
-		;
-	new->len = count;
-	new->next = *head;
-	*head = new;
-	return (*head);
 }
