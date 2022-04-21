7812964321d54196225ef84d32b1c065f741093a
Blessing-Tolani <ojoblessing999@gmail.com>
2022-04-21 12:15:14 -0700

    feat: solve task 0

---
+++ 0x12-singly_linked_lists/0-print-list.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+/**
+ * print_list - prints all the element of a list
+ * @h: parameter
+ * Return: number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	hello;
+
+}
---
+++ 0x12-singly_linked_lists/lists.h
@@ -0,0 +1,14 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+typedef struct list_t
+{
+	char *str;
+	unsigned int len;
+	list_t next;
+} list_t;
+
+size_t print_list(const list_t *h);
+
+
+#endif
