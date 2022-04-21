f97c6ff43a82282e5feff222d8b1fffc770763b6
Blessing-Tolani <ojoblessing999@gmail.com>
2022-04-21 22:04:09 +0100

    update

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,27 +1,15 @@
+	SECTION .data
+msg:	db "Hello, Holberton", 0
+fmt:	db "%s", 10, 0
 
-#include "lists.h"
-/**
- * add_node - adds a new node at the beginning of the list
- * @head: pointer  to the beginning
- * @str: str to be duplicated
- * Return: address of the new element or null
- */
-list_t *add_node(list_t **head, const char *str)
-{
-	list_t *new;
-	int count = 0;
+	SECTION .text
+	extern printf
+	global main
+main:
+	mov esi, msg
+	mov edi, fmt
+	mov eax, 0
+	call printf
 
-	new = malloc(sizeof(list_t));
-	if (new == NULL)
-	{
-		printf("Error\n");
-		return (NULL);
-	}
-	new->str = strdup(str);
-	for (count = 0; str[count] != '\0'; count++)
-		;
-	new->len = count;
-	new->next = *head;
-	*head = new;
-	return (*head);
-}
+	mov eax, 0
+	ret
