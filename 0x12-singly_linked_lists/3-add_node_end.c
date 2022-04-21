34fc4b3082c25b0cc8375585b9009c9152157a50
Blessing-Tolani <ojoblessing999@gmail.com>
2022-04-21 22:05:41 +0100

    fix: task 3

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,22 +1,27 @@
 #include "lists.h"
+
 /**
- * add_node_end - adds a new node at the end of the list
- * @head: pointer  to the beginning
- * @str: str to be duplicated
- * Return: address of the new element or null
+ * add_node_end - adds a new node at the end
+ * of a list_t list.
+ * @head: head of the linked list.
+ * @str: string to store in the list.
+ * Return: address of the head.
  */
+
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *new;
-	int count = 0;
+	list_t *new, *temp;
+	size_t nchar;
 
 	new = malloc(sizeof(list_t));
 	if (new == NULL)
-	{
-		printf("Error\n");
 		return (NULL);
-	}
+
 	new->str = strdup(str);
+
+	for (nchar = 0; str[nchar]; nchar++)
+		;
+
 	new->len = nchar;
 	new->next = NULL;
 	temp = *head;
