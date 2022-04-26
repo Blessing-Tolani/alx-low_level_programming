d3d5efa83b788276e8e677ec9998188d580bac42
Blessing-Tolani <ojoblessing999@gmail.com>
2022-04-26 16:59:42 -0700

    feat: more singly linked list tasks

---
+++ 0x013-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,26 @@
+
+#include "lists.h"
+/**
+ * add_node - adds a new node at the beginning of the list
+ * @head: pointer  to the beginning
+ * @str: str to be duplicated
+ * Return: address of the new element or null
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new;
+	int count = 0;
+
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+	{
+		printf("Error\n");
+		return (NULL);
+	}
+	new->str = strdup(str);
+	for (count = 0; str[count] != '\0'; count++)
+		;
+	new->len = count;
+	new->next = *head;
+	*head = new;
+	return (*head);
