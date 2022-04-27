4a858526c5333b5cee7b2671c4e0d53997496b36
Blessing-Tolani <ojoblessing999@gmail.com>
2022-04-26 17:07:33 -0700

    update

--- 0x13-more_singly_linked_lists/_putchar.c
+++ 0x13-more_singly_linked_lists/_putchar.c
@@ -1,26 +1,12 @@
-
-#include "lists.h"
+#include <unistd.h>
 /**
- * add_node - adds a new node at the beginning of the list
- * @head: pointer  to the beginning
- * @str: str to be duplicated
- * Return: address of the new element or null
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
-list_t *add_node(list_t **head, const char *str)
+int _putchar(char c)
 {
-	list_t *new;
-	int count = 0;
-
-	new = malloc(sizeof(list_t));
-	if (new == NULL)
-	{
-		printf("Error\n");
-		return (NULL);
-	}
-	new->str = strdup(str);
-	for (count = 0; str[count] != '\0'; count++)
-		;
-	new->len = count;
-	new->next = *head;
-	*head = new;
-	return (*head);
+	return (write(1, &c, 1));
+}
