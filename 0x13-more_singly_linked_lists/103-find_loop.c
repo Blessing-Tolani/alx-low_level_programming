5f4b6abdedcd4d69bb0fcdd7ec993c24406c11e8
Blessing-Tolani <ojoblessing999@gmail.com>
2022-04-26 17:14:34 -0700

    fix: advanced task 3

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -1,26 +1,15 @@
-
 #include "lists.h"
-/**
- * add_node - adds a new node at the beginning of the list
- * @head: pointer  to the beginning
- * @str: str to be duplicated
- * Return: address of the new element or null
- */
-list_t *add_node(list_t **head, const char *str)
-{
-	list_t *new;
-	int count = 0;
 
-	new = malloc(sizeof(list_t));
-	if (new == NULL)
-	{
-		printf("Error\n");
+/**
+ * find_listint_loop - finds the loop in a linked list
+ * @head: Head of the list
+ *
+ * Return: The address of the node where the loop starts
+ * or NULL if there is no loop
+ */
+listint_t *find_listint_loop(listint_t *head)
+{
+	if (head == NULL)
 		return (NULL);
-	}
-	new->str = strdup(str);
-	for (count = 0; str[count] != '\0'; count++)
-		;
-	new->len = count;
-	new->next = *head;
-	*head = new;
-	return (*head);
+	return (NULL);
+}
